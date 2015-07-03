8c800253a8dbcd9b72baab8d3c1d07f7aed42aad
Cornelis Kruger <kruger.kerneels@gmail.com>
2015-07-03 16:21:36 +0200

    Add lenode with logentries

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var app = express();
+var Logger = require('le_node');
+var log = Logger({
+  token:process.env.logentries_token
+});
 
 app.get('/', function(req, res){
 
-  res.send('hello');
+  // level specific methods like 'info', 'debug', etc.
+  log.info("I'm a Lumberjack and I'm OK")
+
+  // generic log method, also accepts JSON entries
+  log.log("debug", {sleep:"all night", work:"all day"})
+
+  res.send(‘created 2 logs’);
 });
 
 app.listen(process.env.PORT || 4000, function(){
